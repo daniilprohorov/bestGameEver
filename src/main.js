ede81136427be1a627823ee76ff072e97f584cb6
daniilprohorov <daniilprohorov@gmail.com>
2019-07-24 14:50:48 +0300

    переходим на matter js

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@
             update: update
         },
         physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 1000 },
-                debug: false  
+            default: 'matter',
+            matter: {
+                gravity: { y: 1 },
+                enableSleep: true
             }
         }, scale: {
             mode: Phaser.Scale.FIT,
@@ -44,21 +44,26 @@
         const background = map.createStaticLayer("background", tileset, 0, 0);
         const house = map.createStaticLayer("house", tileset, 0, 0);
         const pillar = map.createStaticLayer("pillar", tileset, 0, 0);
-        const box = map.createStaticLayer("box", tileset, 0, 0);
+        const box = map.createDynamicLayer("box", tileset, 0, 0);
         const tree = map.createStaticLayer("tree", tileset, 0, 0);
+
 		ground.setCollisionByProperty({ collides: true }); 
 		box.setCollisionByProperty({ collides: true }); 
 		tree.setCollisionByProperty({ collides: true }); 
 
-        cat = this.physics.add.sprite(2900, 1000, 'cat');
-        // cat.setBounce(0.2);
+        this.matter.world.convertTilemapLayer(ground);
+        this.matter.world.convertTilemapLayer(box);
+        this.matter.world.convertTilemapLayer(tree);
 
-        // cat.setCollideWorldBounds(true);
-        this.physics.world.TILE_BIAS = 40;
-        this.physics.add.collider(cat, ground);
-        this.physics.add.collider(cat, box);
-        this.physics.add.collider(cat, tree);
-        
+        this.matter.world.createDebugGraphic();
+
+        cat = this.matter.add.sprite(2900, 1000, 'cat');
+
+        // this.physics.world.TILE_BIAS = 40;
+        // this.physics.add.collider(cat, ground);
+        // this.physics.add.collider(cat, box);
+        // this.physics.add.collider(cat, tree);
+        //
         this.anims.create({
             key: 'left',
             frames: [ { key: 'cat', frame: 0 } ], //this.anims.generateFrameNumbers('cat', { start: 0, end: 3 }),
@@ -84,50 +89,53 @@
             frameRate: 10,
             //repeat: -1
         });
-
-
+        //
+        //
         const camera = this.cameras.main;
         camera.startFollow(cat, false, 0.05, 0.5, -160, 250);
         camera.setDeadzone(400, 800);
-        camera.centerOn(2000, 1000);
+        camera.centerOn(2000, 3000);
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         camera.roundPixels= true;
-
+        //
         cursors = this.input.keyboard.createCursorKeys();
 
     }
     function update ()
     {
-
-        // for going left and right 
+        // for going left and right
         if (cursors.left.isDown) {
-            cat.setVelocityX(-250);
+            cat.setVelocityX(-5);
             cat.anims.play('left', true);
         }
         else if (cursors.right.isDown) {
-            cat.setVelocityX(250);
+            cat.setVelocityX(5);
             cat.anims.play('right', true);
         }
         else {
             cat.setVelocityX(0);
             cat.anims.play('def', true);
         }
-        
+
         // for jumping
-        if (cursors.up.isDown && cat.body.onFloor()){
-            cat.setVelocityY(-1000);
+        // if (cursors.up.isDown && cat.body.onFloor()){
+        //     cat.setVelocityY(-10);
+        //     // cat.anims.play('turn', true);
+        // }
+        if (cursors.up.isDown){
+            cat.setVelocityY(-10);
             cat.anims.play('turn', true);
         }
         // for jumping from walls
-        else if(cat.body.onWall() && cursors.up.isDown) {
-            if(cursors.left.isDown) {
-                cat.setVelocity(-150, -700);
-            }
-            else if (cursors.right.isDown) {
-                cat.setVelocity(150, -700);
-            }
-
-        }
+        // else if(cat.body.onWall() && cursors.up.isDown) {
+        //     if(cursors.left.isDown) {
+        //         cat.setVelocity(-15, -7);
+        //     }
+        //     else if (cursors.right.isDown) {
+        //         cat.setVelocity(15, -7);
+        //     }
+        //
+        // }
 
         
         
