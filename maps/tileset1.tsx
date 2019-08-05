1a0713ed9e6b594d69c418b5e336ace4be88bd53
RitaPochueva <53226355+RitaPochueva@users.noreply.github.com>
2019-08-05 18:04:26 +0400

    Много всего

---
+++ maps/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset1" tilewidth="200" tileheight="200" tilecount="165" columns="11">
+ <image source="../res/tileset1.png" width="2200" height="3000"/>
+</tileset>
---
+++ maps/tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset3" tilewidth="200" tileheight="200" tilecount="225" columns="15">
+ <image source="../res/tileset3.png" width="3000" height="3000"/>
+</tileset>
