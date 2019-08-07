0e347a542a4d7cdd994a8dcf04e0da24488d8407
RitaPochueva <53226355+RitaPochueva@users.noreply.github.com>
2019-08-07 16:01:21 +0400

    карта и много изменений

--- maps/tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tileset2" tilewidth="200" tileheight="200" tilecount="225" columns="15">
- <image source="../res/tileset2.png" width="3000" height="3000"/>
-</tileset>
